b6ab774a606e995dac921408578125f57c5bb1d3
abdxu9 <abdoulatufcolo@outlook.fr>
2024-10-12 16:01:08 +0200

    Add function to seeker type of park and return an json file

--- space_e_hackhathon/static/js/script.js
+++ space_e_hackhathon/static/js/script.js
@@ -1,11 +1,29 @@
-const url = 'https://opendata.clermontmetropole.eu/api/records/1.0/search/?dataset=parcs-et-jardins-ville-de-clermont-ferrand&rows=10&sort=type';
-    
-fetch(url, {
+const url = 'https://opendata.clermontmetropole.eu/api/explore/v2.1/catalog/datasets/parcs-et-jardins-ville-de-clermont-ferrand/records?';
+let type = ["Espace vert public de proximité","Parc urbain public"];
+        
+function whereSingle(text) {
+    return `where=%22${text}%22`;
+}
+
+// Function to create the where clause for multiple types
+function whereMultiple(types, operator = "OR") {
+    // Map each type to its URL-encoded representation and join them with the specified operator
+    const conditions = types.map(text => `%22${text}%22`).join(` ${operator} `);
+    return `where=${conditions}`;
+}
+
+
+fetch(url +whereMultiple(type), {
     method: 'GET',
     mode: 'cors'
 })
+
 .then(function(response) {
+if (response.headers.get('content-type') && response.headers.get('content-type').includes('application/json')) {
     return response.json(); // Parse the response as JSON
+} else {
+    return response.text(); // Fallback in case it's not JSON
+}
 })
 .then(function(data){
     console.log(data); // Check the returned JSON data
